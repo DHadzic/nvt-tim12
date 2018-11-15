27bc1f2d7b946cc52c0c76075a04062878d607cc
DHadzic <damirhadzic996@gmail.com>
2018-11-15 17:11:35 +0100

    Registracija

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -1,9 +1,93 @@
 package com.project.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserController {
-	
+import com.project.domain.Passenger;
+import com.project.domain.Ticket;
+import com.project.repository.UserRepository;
+import com.project.security.TokenUtils;
+import com.project.web.dto.LoginDTO;
+import com.project.web.dto.PassengerDTO;
 
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	TokenUtils tokenUtils;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
+        try {
+        	// Perform the authentication
+        	UsernamePasswordAuthenticationToken token = 
+        			new UsernamePasswordAuthenticationToken(
+					loginDTO.getUsername(), loginDTO.getPassword());
+            Authentication authentication = authenticationManager.authenticate(token);            
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            // Reload user details so we can generate token
+            UserDetails details = userDetailsService.
+            		loadUserByUsername(loginDTO.getUsername());
+            return new ResponseEntity<String>(
+            		tokenUtils.generateToken(details), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<String>("Invalid login", HttpStatus.BAD_REQUEST);
+        }
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<String> register(@RequestBody PassengerDTO passengerDTO) {
+		
+		if(userRepository.findByUsername(passengerDTO.getUsername()) == null) {
+			return new ResponseEntity<String>("Invalid register",HttpStatus.BAD_REQUEST);
+		}
+		
+		Passenger passenger = new Passenger();
+		passenger.setUsername(passengerDTO.getUsername());
+		passenger.setPassword(passengerDTO.getPassword());
+		passenger.setName(passengerDTO.getName());
+		passenger.setSurname(passenger.getSurname());
+		passenger.setType(passengerDTO.getType());
+		passenger.setBirthDate(passengerDTO.getBirthDate());
+		passenger.setTikcet(new ArrayList<Ticket>());
+		
+		userRepository.save(passenger);
+		
+    	UsernamePasswordAuthenticationToken token = 
+    			new UsernamePasswordAuthenticationToken(
+				passenger.getUsername(), passenger.getPassword());
+        Authentication authentication = authenticationManager.authenticate(token);            
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Reload user details so we can generate token
+        UserDetails details = userDetailsService.
+        		loadUserByUsername(passenger.getUsername());
+
+        return new ResponseEntity<String>(
+				tokenUtils.generateToken(details),HttpStatus.OK);
+	}
 }
---
+++ src/main/java/com/project/web/dto/PassengerDTO.java
@@ -0,0 +1,52 @@
+package com.project.web.dto;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+
+import com.project.domain.PassengerType;
+
+public class PassengerDTO {
+	private String username;
+	private String password;
+	private String name;
+	private String surname;
+	private Date birthDate;
+	private PassengerType type;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public PassengerType getType() {
+		return type;
+	}
+	public void setType(PassengerType type) {
+		this.type = type;
+	}
+}
