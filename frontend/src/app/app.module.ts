f8510e1633a024b5da747cc805db7407a980802b
DHadzic <damirhadzic996@gmail.com>
2019-01-07 15:41:07 +0100

    ZNAM DA KORISTIM MERGING TOOL :)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,13 +14,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AuthenticationService } from './services/security/authentication-service.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptorService } from './services/security/token-interceptor.service';
-<<<<<<< HEAD
 import { RegisterComponent } from './components/register/register.component'
 import { LoginGuardGuard } from './services/security/login-guard.guard';
-=======
-import { RegisterComponent } from './components/register/register.component';
 import { TicketsComponent } from './components/tickets/tickets.component'
->>>>>>> branch 'master' of https://github.com/DHadzic/nvt-tim12
 
 const appRoutes: Routes = [
   { path: 'main', 
