3f763782a6d38c08fee3da06060b25fe31760302
DHadzic <damirhadzic996@gmail.com>
2019-01-28 23:46:39 +0100

    3 od 7 testiranih front komponenti.

--- frontend/src/app/components/register/register.component.spec.ts
+++ frontend/src/app/components/register/register.component.spec.ts
@@ -1,14 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import {} from 'jasmine';
 
 import { RegisterComponent } from './register.component';
+import { FormsModule } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 
-describe('RegisterComponent', () => {
+fdescribe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let userService: any;
+  let router: any;
 
   beforeEach(async(() => {
+    let userService = {
+      register : jasmine.createSpy('register').and.returnValue(of([]))
+    }
+
+    let routerMock = {
+      navigate: jasmine.createSpy('navigate')
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      imports : [ FormsModule ],
+      declarations: [ RegisterComponent ],
+      providers: [ { provide: UserService , useValue: userService},
+                  { provide: Router , useValue: routerMock} ]
     })
     .compileComponents();
   }));
@@ -16,10 +35,85 @@ describe('RegisterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    userService = TestBed.get(UserService);
+    router = TestBed.get(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set error properties to true',() =>{
+    component.passenger.username = "a";
+    component.passenger.password = "a";
+    component.passenger.password_r = "aa";
+    component.passenger.name = "a";
+    component.passenger.surname = "a";
+    component.passenger.birthDate = "12-12-1000";
+
+    component.handleValidation();
+
+    expect(component.error_messages.username).toBe(true);
+    expect(component.error_messages.password).toBe(true);
+    expect(component.error_messages.password_r).toBe(true);
+    expect(component.error_messages.name).toBe(true);
+    expect(component.error_messages.surname).toBe(true);
+    expect(component.error_messages.birthDate).toBe(true);
+  });
+
+  it('should set error properties to true 2',() =>{
+    component.passenger.username = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    component.passenger.password = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    component.passenger.password_r = "a";
+    component.passenger.name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    component.passenger.surname = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    component.passenger.birthDate = "12-12-3000";
+
+    component.handleValidation();
+
+    expect(component.error_messages.username).toBe(true);
+    expect(component.error_messages.password).toBe(true);
+    expect(component.error_messages.password_r).toBe(true);
+    expect(component.error_messages.name).toBe(true);
+    expect(component.error_messages.surname).toBe(true);
+    expect(component.error_messages.birthDate).toBe(true);
+  });
+
+  it('should set error properties to false',() =>{
+    component.passenger.username = "aaaaaaaaaaaa";
+    component.passenger.password = "aaaaaaaaaaaa";
+    component.passenger.password_r = "aaaaaaaaaaaa";
+    component.passenger.name = "aaaaaaaaaaaa";
+    component.passenger.surname = "aaaaaaaaaaaa";
+
+    component.handleValidation();
+
+    expect(component.error_messages.username).toBe(false);
+    expect(component.error_messages.password).toBe(false);
+    expect(component.error_messages.password_r).toBe(false);
+    expect(component.error_messages.name).toBe(false);
+    expect(component.error_messages.surname).toBe(false);
+  });
+
+  it('should call service set properties',() =>{
+    spyOn(component, 'handleValidation').and.returnValue(true);
+    component.passenger.username = "aaaaaaaaaaaa";
+    component.passenger.password = "aaaaaaaaaaaa";
+    component.passenger.password_r = "aaaaaaaaaaaa";
+    component.passenger.name = "aaaaaaaaaaaa";
+    component.passenger.surname = "aaaaaaaaaaaa";
+    expect(component.error_messages.username).toBe(false);
+    expect(component.error_messages.password).toBe(false);
+    expect(component.error_messages.password_r).toBe(false);
+    expect(component.error_messages.name).toBe(false);
+    expect(component.error_messages.surname).toBe(false);
+
+    component.register();
+
+    expect(component.handleValidation).toHaveBeenCalled();
+    expect(userService.register).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(["/main"]);
+  });
+
 });
--- frontend/src/app/pages/main-page/main-page.component.spec.ts
+++ frontend/src/app/pages/main-page/main-page.component.spec.ts
@@ -1,14 +1,77 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Observable } from "rxjs";
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MapService } from 'src/app/services/map.service';
 
 import { MainPageComponent } from './main-page.component';
+import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';
+import { componentNeedsResolution } from '@angular/core/src/metadata/resource_loading';
 
-describe('MainPageComponent', () => {
+fdescribe('MainPageComponent', () => {
   let component: MainPageComponent;
   let fixture: ComponentFixture<MainPageComponent>;
+  let mapService: any;
+  let router: any;
 
   beforeEach(async(() => {
+    let mapService = {
+      getLinesPerType : jasmine.createSpy('getLinesPerType').and.returnValue(of({
+        "line": {
+            "id": 999,
+            "name": "8a",
+            "stations": [
+                {
+                    "id": 999,
+                    "lat": "45.264054514190796",
+                    "lng": "19.83022916394043"
+                },
+                {
+                    "id": 998,
+                    "lat": "45.26042973161276",
+                    "lng": "19.832632423217774"
+                },
+                {
+                    "id": 997,
+                    "lat": "45.25148761176708",
+                    "lng": "19.837610603149415"
+                },
+                {
+                    "id": 996,
+                    "lat": "45.25252355951289",
+                    "lng": "19.847634710688453"
+                }
+            ]
+        },
+        "atStations": [
+            1
+        ]
+    })),
+      getLineInfo : jasmine.createSpy('getLineInfo').and.returnValue(of({
+        "tramLines": [
+            "8a"
+        ],
+        "busLines": [
+            "8a",
+            "8b"
+        ],
+        "trolleybusLines": []
+      }))
+    }
+
+    let routerMock = {
+      navigate: jasmine.createSpy('navigate')
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ MainPageComponent ]
+      imports : [ FormsModule, AgmCoreModule.forRoot({
+        apiKey: 'AIzaSyBG-Cyqxezk9fYYllu_VmEe8q3ZBFf30Yc'
+      }) , AgmDirectionModule],
+      declarations: [ MainPageComponent ],
+      providers: [ { provide: MapService , useValue: mapService},
+                  { provide: Router , useValue: routerMock} ]
     })
     .compileComponents();
   }));
@@ -16,10 +79,53 @@ describe('MainPageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MainPageComponent);
     component = fixture.componentInstance;
+    mapService = TestBed.get(MapService);
+    router = TestBed.get(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load data on init',()=>{
+    expect(mapService.getLinesPerType).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call service if good data',()=>{
+    component.selectedType = "Bus";
+    component.selectedLineBus = "Line1";
+    
+    component.displayLine();
+
+    expect(mapService.getLineInfo).toHaveBeenCalled();
+  });
+
+  it('should call service if good data2',()=>{
+    component.selectedType = "Tram";
+    component.selectedLineTram = "Line1";
+    
+    component.displayLine();
+
+    expect(mapService.getLineInfo).toHaveBeenCalled();
+  });
+
+  it('should call service if good data3',()=>{
+    component.selectedType = "Trolleybus";
+    component.selectedLineTrolleybus = "Line1";
+    
+    component.displayLine();
+
+    expect(mapService.getLineInfo).toHaveBeenCalled();
+  });
+
+  it('should not call service if bad data',()=>{
+    component.selectedType = "Trolleybus";
+    component.selectedLineBus = undefined;
+    
+    component.displayLine();
+
+    expect(mapService.getLineInfo).not.toHaveBeenCalled();
+  });
+
 });
